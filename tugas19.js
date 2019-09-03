eee1d280cd4ee274100ef46ffaace57a7608dee3
noorqidam <noorqidam@gmail.com>
2019-09-03 15:11:24 +0700

    Tugas 19 JS
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas19.js
@@ -0,0 +1,13 @@
+function repeat() {
+  console.log("Semangat...".repeat(5));
+  console.log("*".repeat(5));
+  console.log("*".repeat(25));
+  console.log("*".repeat(50));
+  console.log("*".repeat(75));
+  console.log("*".repeat(100));
+  console.log("*".repeat(75));
+  console.log("*".repeat(50));
+  console.log("*".repeat(25));
+  console.log("*".repeat(5));
+}
+repeat();
